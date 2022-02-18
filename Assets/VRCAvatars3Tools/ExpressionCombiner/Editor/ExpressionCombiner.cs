5b2d9da8be64a22ee1134af17cb1c94725f29c7c
gatosyocora <gatosyocora36@gmail.com>
2022-02-18 23:36:27 +0900

    ExpressionParameterとMenuを別のものと合成するツールのファイルを作成

---
+++ Assets/VRCAvatars3Tools/ExpressionCombiner/Editor/ExpressionCombiner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace Gatosyocora.VRCAvatars3Tools
+{
+    public class ExpressionCombiner : EditorWindow
+    {
+        [MenuItem("VRCAvatars3Tools/ExpressionCombiner")]
+        public static void Open()
+        {
+            GetWindow<ExpressionCombiner>("ExpressionCombiner");
+        }
+
+        public void OnGUI()
+        {
+        }
+    }
+}
