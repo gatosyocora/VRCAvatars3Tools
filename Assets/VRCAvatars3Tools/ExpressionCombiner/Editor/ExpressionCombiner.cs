ea82f389c0c0d61a655bfd293038ddc107cf8a64
gatosyocora <gatosyocora36@gmail.com>
2022-02-19 21:08:16 +0900

    コピー元のExpressionParameterを設定する項目を追加

--- Assets/VRCAvatars3Tools/ExpressionCombiner/Editor/ExpressionCombiner.cs
+++ Assets/VRCAvatars3Tools/ExpressionCombiner/Editor/ExpressionCombiner.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEditor;
+using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace Gatosyocora.VRCAvatars3Tools
 {
     public class ExpressionCombiner : EditorWindow
     {
+        private VRCExpressionParameters srcParameters;
+
         [MenuItem("VRCAvatars3Tools/ExpressionCombiner")]
         public static void Open()
         {
@@ -12,6 +15,13 @@ namespace Gatosyocora.VRCAvatars3Tools
 
         public void OnGUI()
         {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                srcParameters = EditorGUILayout.ObjectField("Src ExpressionParameters", srcParameters, typeof(VRCExpressionParameters), true) as VRCExpressionParameters;
+                if (check.changed)
+                {
+                }
+            }
         }
     }
 }
